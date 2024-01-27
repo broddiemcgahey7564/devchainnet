530143c1e94a6c46210e0d2f16931504d4366643
ruzgarataozkan <devchainnet@gmail.com>
2024-01-27 08:01:15 +0300

    todos add

--- src/index.js
+++ src/index.js
@@ -11,3 +11,9 @@ export default {
   Graph,
   utils,
 };
+
+// TODO: parse index.css and split components module styles into different css files so user can import seperatly for performance reasons.
+
+// TODO: Inject each css file into its own component in componentDidMount with   const style = document.createElement('style'); style.textContent = styleString; document.head.append(style);
+
+// TODO: fix axios.default bug fix
